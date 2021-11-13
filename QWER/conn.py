0eba8405e745be9b1e8785369fefe2f7e517406b
kokkin1 <kokkin.george@gmail.com>
2021-11-13 09:18:11 +0200

    com1

---
+++ QWER/conn.py
@@ -0,0 +1,12 @@
+import os
+import sqlite3
+
+
+def execute_query(query):
+    path2db = os.path.join(os.getcwd(), "example.sqlite3")
+    connection = sqlite3.connect(path2db)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    res = cursor.fetchall()
+    return res
