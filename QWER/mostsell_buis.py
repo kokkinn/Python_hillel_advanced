1e4ad92e98cd5a83798b4f8f21fec6381a79467e
kokkin1 <kokkin.george@gmail.com>
2021-11-13 09:48:34 +0200

    c0mm1t

--- QWER/mostsell_buis.py
+++ QWER/mostsell_buis.py
@@ -1,16 +1,7 @@
-from QWER.conn import execute_query
-from pprint import pprint
-
-#
-# #
-# sql1 = execute_query("select Trackid, Name from tracks")
-# sql2 = execute_query("select trackid, unitprice, quantity from invoice_items")
-# #
-# num = 4
 def formatR(qry1, qry2, num=None):
     qry2.sort(key=lambda x: x[2])
     a = 0
-    # qry2 = [list(x) for x in qry2]
+
     qry2 = qry2[:num]
     dic1 = {}
     for i in qry2:
@@ -18,12 +9,4 @@ def formatR(qry1, qry2, num=None):
         a += 1
     dic2 = "<br>".join(" ".join((k, v)) for (k, v) in dic1.items())
 
-    # qry2 = tuple([tuple(x) for x in qry2])
-    # u = " ".join(qry2)
-
-    # x = "<br>".join(qry2)
     return dic2
-#
-# print(formatR(sql1, sql2, 4))
-
-# print(formatR(sql1, sql2, num))
--- QWER/mostsell_web.py
+++ QWER/mostsell_web.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 
 @app.route("/count")
-@use_kwargs({"num": fields.Int(required=False)}, location="query")
+@use_kwargs({"num": fields.Int(required=False, validate=\
+    lambda x: 0 < x < len(execute_query("select trackid, unitprice, quantity from invoice_items")))}, location="query")
 def countt(num):
     sql1 = execute_query("select Trackid, Name from tracks")
     sql2 = execute_query("select trackid, unitprice, quantity from invoice_items")
-
     a = formatR(sql1, sql2, num)
     return a
 
