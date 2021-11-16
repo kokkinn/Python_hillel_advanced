9b3472313caa65913e2f3a62e81e45c656e62f70
kokkin1 <kokkin.george@gmail.com>
2021-11-16 19:00:54 +0200

    test

--- flask_hmWRK_7_sort/mostsell_buis.py
+++ flask_hmWRK_7_sort/mostsell_buis.py
@@ -8,15 +8,15 @@
 def formatR(qry1, qry2, num=None):
     qry2.sort(key=lambda x: x[2])
     a = 0
-
     qry2 = qry2[:num]
+
     # dic1 = {}
     # for i in qry2:
-    #     dic1[qry1[a][1]] = str(i[1] * i[2]) + " " + str(i[2])
+    #     dic1[qry1[a][1]] = str(i[1] * i[2]) + " " + str(i[2])         #другой способ
     #     a += 1
     # dic2 = "<br>".join(" ".join((k, v)) for (k, v) in dic1.items())
+
     qr = [list(i) for i in qry2]
-    a = 0
     for f in qr:
         f[0] = qry1[a][1]
         f[1] = f[1] * f[2]
@@ -25,6 +25,3 @@ def formatR(qry1, qry2, num=None):
 
     x = "<br>".join(map(lambda x: str(x[0]) + ' ' + str(x[1]) + " " + str(x[2]), qr))
     return x
-    # return dic2
-
-# print(formatR(sql1, sql2, num))
--- flask_hmWRK_8_joinSQL/web.py
+++ flask_hmWRK_8_joinSQL/web.py
@@ -14,4 +14,15 @@ def track_len():
     return '<br>'.join(map(lambda x: str(x[0]) + ' ' + str(x[1]), sql))
 
 
+@app.route("/track_sort")
+@use_kwargs({"num": fields.Int(required=False, validate= \
+        lambda x: 0 <= x <= len(execute_query("select trackid, unitprice, quantity from invoice_items")))},
+            location="query")
+def track_sort(num=None):
+    sql = execute_query("SELECT Name, ii.UnitPrice * ii.Quantity as Sum_of_sells, ii.Quantity from tracks t join "
+                        "invoice_items ii ON t.TrackId = ii.TrackId order by ii.Quantity")
+    sql = sql[:num]
+    return '<br>'.join(map(lambda x: str(x[0]) + ' ' + str(x[1]) + " " + str(x[2]), sql))
+
+
 app.run(debug=True)
