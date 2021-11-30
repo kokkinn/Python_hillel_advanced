767caca3df6ac24689565534f07e937ebc8ad4e8
kokkin1 <kokkin.george@gmail.com>
2021-11-30 16:17:10 +0200

    hmwrk9rework1

--- OOP_hmWRK_9_point/point.py
+++ OOP_hmWRK_9_point/point.py
@@ -10,21 +10,20 @@ class Point:
         res = Point(self.x + other.x, self.y + other.y)
         return res
 
+    def __sub__(self, other):
+        res = Point(self.x - other.x, self.y - other.y)
+        return res
+
     def __str__(self):
         return str(self.x) + " " + str(self.y)
 
 
-p1 = Point(4, 2)
-p2 = Point(3, 1)
-p3 = Point(2, 2)
-
-
 class Triangle:
 
-    def __init__(self):
-        self.p1 = p1
-        self.p2 = p2
-        self.p3 = p3
+    def __init__(self, p01, p02, p03):
+        self.p1 = p01
+        self.p2 = p02
+        self.p3 = p03
 
     def area(self):
         area = 1 / 2 * abs(
@@ -44,18 +43,22 @@ class Triangle:
         self.p1.y = newy
 
     def editP2(self, newx, newy):
-        self.p1.x = newx
-        self.p1.y = newy
+        self.p2.x = newx
+        self.p2.y = newy
 
     def editP3(self, newx, newy):
-        self.p1.x = newx
-        self.p1.y = newy
+        self.p3.x = newx
+        self.p3.y = newy
 
 
-tr1 = Triangle()
-print(tr1.area())
-print(tr1.perimetr())
-tr1.editP1(2, 1)
-tr1.editP2(2, 3)
-print(tr1.p1.x)
-print(tr1.p2.y)
+p1 = Point(4, 2)
+p2 = Point(3, 1)
+p3 = Point(2, 2)
+tr1 = Triangle(p1, p2, p3)
+# print(tr1.area())
+# print(tr1.perimetr())
+# print(p3+p2)
+# print(p3-p2)
+# print(tr1.p1, tr1.p2, tr1.p3)
+# tr1.editP1(10, 12)
+# print(tr1.p1, tr1.p2, tr1.p3)
